af5bb15b57ce07ab62c2693d6cc4473e80c57530
osenpb <oeperezbellido@gmail.com>
2025-10-10 11:14:20 -0500

    arreglos para empaquetado

--- src/main/java/com/osen/back_estado_metro/services/impl/UserServiceImpl.java
+++ src/main/java/com/osen/back_estado_metro/services/impl/UserServiceImpl.java
@@ -42,7 +42,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(Long id) {
-        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(STR."Usuario con id: '\{id}' no encontrado"));
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException("Usuario con id: " + id + "no encontrado"));
     }
 
     @Override
@@ -64,6 +64,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException(STR."Usuario con email '\{email}' no encontrado"));
+                .orElseThrow(() -> new UserNotFoundException("Usuario con email " + email + "no encontrado"));
     }
 }
